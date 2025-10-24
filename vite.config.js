fdbfb17a510da99835c8b79d7a5c32d0d157a1a1
Corentin Delvaque <corentin.delvaque@icloud.com>
2025-10-24 09:55:07 +0200

    fifth commit

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   plugins: [tailwindcss()],
-  base: "/Initialisation_Tailwind",
+  base: "/SAE301-Corentin-Delvaque",
 });
 
 //npm install
