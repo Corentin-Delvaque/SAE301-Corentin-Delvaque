133ea708fbd33918509b326b337e4d5e0695a6de
Corentin Delvaque <corentin.delvaque@icloud.com>
2025-10-24 16:40:36 +0200

    nineth commit

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,19 @@
 import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
+import { resolve } from "path";
 
 export default defineConfig({
   plugins: [tailwindcss()],
   base: "/SAE301-Corentin-Delvaque",
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        plus: resolve(__dirname, "pages/boutique.html"),
+        // ajoutez d'autres pages si nécessaire
+      },
+    },
+  },
 });
 
 //npm install
